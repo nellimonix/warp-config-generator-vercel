1e32bc25a30cdd0a0493d9d6fb290e0e6c8da078
nellimonix <llimonix123@ya.ru>
2025-06-11 15:06:58 +0300

    Set peer endpoint to a static IP address.

--- lib/warpConfig.ts
+++ lib/warpConfig.ts
@@ -143,10 +143,12 @@ DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
 [Peer]
 PublicKey = ${peer_pub}
 AllowedIPs = ${allowed_ips}
-Endpoint = ${peer_endpoint}`
+Endpoint = 188.114.99.224:1002`
 
   return conf
 }
+
+// ${peer_endpoint}
 
 function removeMtuLine(config: string) {
   return config.replace(/^MTU = 1280\n?/gm, "")
