b542023f741fc7de1b68607291d9ffac0fc8de8b
nellimonix <llimonix123@ya.ru>
2025-08-20 22:01:25 +0300

    feat: Improves UI with new icons and AmneziaWG 1.5 mobile internet alert.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
+import {
+  Bot,
+  Rss,
+  FolderOpen,
+  Info,
+} from "lucide-react"
+import { RiTelegram2Line, RiGithubLine, RiRobot2Line } from "react-icons/ri";
 import { WarpGenerator } from "@/components/warp-generator"
 import { Analytics } from "@vercel/analytics/next"
 
@@ -9,20 +16,19 @@ export default function Home() {
     <>
       <Analytics/>
       <main className="flex min-h-screen flex-col items-center justify-center p-4 w-full">
-        <Alert className="alert mb-6 break-words" style={{ display: "none" }}>
-          <AlertTitle>Mirrors</AlertTitle>
-          <AlertDescription className="break-words">
-            Сделал сайт с зеркалами этого сайта на случай, если он перестанет работать:{" "}
-            <a href="https://warp-mirrors.vercel.app" className="font-medium">
-              warp-mirrors.vercel.app
-            </a>
-          </AlertDescription>
-        </Alert>
         <Alert className="alert mb-6 break-words">
-          <AlertTitle>Новый способ починить WARP на мобильном интернете с помощью AWG 1.5!</AlertTitle>
-          <AlertDescription className="break-words">
-            Добавлен новый параметр AmneziaWG 1.5, который работает только в приложении AmneziaVPN и работает на мобильных устройствах:{" "}
-            <a href="https://t.me/findllimonix/55" className="font-medium">
+          <AlertTitle className="flex items-center gap-2">
+            <Info className="w-5 h-5 text-gray-400" />
+            WARP на мобильном интернете
+          </AlertTitle>
+
+          <AlertDescription className="break-words mt-3">
+            Добавлен новый параметр <strong>AmneziaWG 1.5</strong>, который работает только
+            в приложении <strong>AmneziaVPN</strong> и на мобильных устройствах:{" "}
+            <a
+              href="https://t.me/findllimonix/55"
+              className="font-medium text-gray-400 hover:underline"
+            >
               инструкция по настройке
             </a>
           </AlertDescription>
@@ -30,14 +36,14 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center gap-6 w-[300px]">
           <Image src="/logo.svg" alt="Логотип" width={300} height={300}/>
           <WarpGenerator/>
-          <Button asChild className="w-full">
-            <a href="https://t.me/warp_generator_bot">Warp Generator Bot</a>
+          <Button asChild variant="secondary" className="w-full">
+            <a href="https://t.me/warp_generator_bot" target="_blank"><RiRobot2Line />Warp Generator Bot</a>
           </Button>
-          <Button asChild className="w-full">
-            <a href="https://t.me/+fDYczngHDFplZDli">Telegram канал</a>
+          <Button asChild variant="secondary" className="w-full">
+            <a href="https://t.me/+fDYczngHDFplZDli" target="_blank"><RiTelegram2Line />Telegram канал</a>
           </Button>
-          <Button asChild className="w-full">
-            <a href="https://github.com/nellimonix/warp-config-generator-vercel">GitHub репозиторий</a>
+          <Button asChild variant="secondary" className="w-full">
+            <a href="https://github.com/nellimonix/warp-config-generator-vercel" target="_blank"><RiGithubLine />GitHub репозиторий</a>
           </Button>
           <p className="text-sm text-muted-foreground">
             Пожалуйста, поддержите меня на GitHub, поставив звезду.
