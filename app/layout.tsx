d193c04274f1896d1d030c46f6733c50afdd0a5e
nellimonix <llimonix123@ya.ru>
2025-08-30 19:32:09 +0300

    feat: Integrate Rybbit Analytics and Yandex Metrika with components

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 import type React from "react"
 import Script from "next/script"
 import { Analytics } from "@vercel/analytics/next"
+import AnalyticsLoader from "@/components/rybbit-analytics"
+import YandexMetrikaLoader from "@/components/yandex-metrika"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -30,41 +32,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ru">
       <head>
         <Analytics/>
-        <Script
-          src="https://analytics.llimonix.pw/api/script.js"
-          data-site-id="1"
-          strategy="afterInteractive"
-        />
+        <AnalyticsLoader/>
+        <YandexMetrikaLoader/>
         <link rel="icon" href="/cloud.ico" type="image/x-icon" />
-        {/* Подключаем Яндекс.Метрику */}
-        <Script
-          id="yandex-metrika"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-              ym(98811523, "init", {
-                defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-              });
-            `,
-          }}
-        />
-        <noscript>
-          <div>
-            <img src="https://mc.yandex.ru/watch/98811523" style={{ position: "absolute", left: "-9999px" }} alt="" />
-          </div>
-        </noscript>
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
