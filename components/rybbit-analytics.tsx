6f82534780cec2ad47b1f2114f77613ed0faa8ab
nellimonix <llimonix123@ya.ru>
2026-01-27 20:47:13 +0300

    feat: add site ID for warp.llimonix.pw in analytics loader

--- components/rybbit-analytics.tsx
+++ components/rybbit-analytics.tsx
@@ -14,6 +14,7 @@ export default function AnalyticsLoader() {
     else if (hostname === "getwarp.netlify.app") setSiteId("2")
     else if (hostname === "getwarp.pages.dev") setSiteId("3")
     else if (hostname === "warply.vercel.app") setSiteId("4")
+    else if (hostname === "warp.llimonix.pw") setSiteId("5d2e1371719c")
     else setSiteId(null) // аналитику не добавляем
 
   }, [])
